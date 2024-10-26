848f3cd27adcbd9894e2ef9fa28467d6fcb08412
CliffHanger201 <sandy.hay@ntlworld.com>
2024-10-26 16:22:06 +0100

    FileEditor

---
+++ src/main/java/cards/FileEditor.java
@@ -0,0 +1,56 @@
+package cards;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class FileEditor {
+    public void createFile(String fileName){
+        try {
+            File myObj = new File(fileName);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
+
+    public void writeFile(String fileName, String fileContent){
+        try{
+            File file = new File(fileName);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(fileContent);
+            writer.newLine();
+            writer.close();
+        } catch (FileNotFoundException e){
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            System.out.println("An error occured.");
+        }
+    }
+
+    public String[] ReadFile(String fileName){
+        try{
+            File file = new File(fileName);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            ArrayList<String> lines = new ArrayList<String>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            return lines.toArray(String[]::new);
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            System.out.println("An error occured.");
+        }
+    }
+}

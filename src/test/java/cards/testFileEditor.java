db6c20265c690d5655a2fd24b26f56cb251570df
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2024-10-26 16:30:30 +0100

    FileEditor Testing

---
+++ src/test/java/cards/testFileEditor.java
@@ -0,0 +1,28 @@
+package cards;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.File;
+import java.io.IOException;
+
+public class testFileEditor {
+
+    @Test
+    @DisplayName("Test File Creation")
+    void testFileCreation() {
+        FileEditor fe = new FileEditor();
+        fe.createFile("testFile.txt");
+        File f = new File("testFile.txt");
+        Assertions.assertTrue(f.exists(), "File was not created");
+    }
+    @Test
+    @DisplayName("Test Write and Read File")
+    void testFileWriting() {
+        FileEditor fe = new FileEditor();
+        fe.createFile("testFile.txt");
+        fe.writeFile("testFile.txt", "Test Data");
+        Assertions.assertEquals(fe.readFile("testFile.txt")[0], "Test Data", "File was not written to or read from correctly");
+
+    }
+}

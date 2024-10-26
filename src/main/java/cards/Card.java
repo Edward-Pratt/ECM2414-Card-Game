e643a35b53a2d6e79b73529c66a6e7d9a514e304
CliffHanger201 <sandy.hay@ntlworld.com>
2024-10-26 18:43:52 +0100

    changes

--- src/main/java/cards/Card.java
+++ src/main/java/cards/Card.java
@@ -1,5 +1,13 @@
 package cards;
 
 public class Card {
+    private final int value;
 
+    public Card(int value){
+        this.value = value;
+    }
+    
+    public int getValue(){
+        return this.value;
+    }
 }

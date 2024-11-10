27b93619ec3410c70a5b5d0692e5586b6e66e446
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2024-11-10 15:47:24 +0000

    pom.xml and CardGame.java Changes

--- src/main/java/cards/CardGame.java
+++ src/main/java/cards/CardGame.java
@@ -1,8 +1,19 @@
 package cards;
 
+import java.util.Scanner;
+
 public class CardGame {
     public static void main(String[] args) {
-        
+        try{
+            Scanner commandReader = new Scanner(System.in);
+            System.out.println("Welcome to the card game! Please enter the number of players: ");
+            int numPlayers = commandReader.nextInt();
+            System.out.println("Please enter the directory of your pack file: ");
+            String packFile = commandReader.next();
+            System.out.println(("You are playing with "+ numPlayers+ " players and the pack file"+ packFile));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
     public static void playGame(){
         

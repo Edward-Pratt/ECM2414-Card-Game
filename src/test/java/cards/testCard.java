f3a8dd2ee3e7aa3d4e55e9ae70db6827ebe21b35
CliffHanger201 <sandy.hay@ntlworld.com>
2024-11-10 16:18:50 +0000

    insert pack test class

--- src/test/java/cards/testCard.java
+++ src/test/java/cards/testCard.java
@@ -1,13 +1,25 @@
 package cards;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class testCard {
 
+    int testCardNumber;
+    Card card;
+
+    @BeforeEach
+    @DisplayName("Card Generation")
+    void generateCard(){
+        this.testCardNumber = (int) Math.round(Math.random() * 100);
+        this.card = new Card(this.testCardNumber);
+    }
+
     @Test
-    @DisplayName("Card Creation")
-    void testCardCreation(){
-        
+    @DisplayName("Card Number")
+    void testCardNumber(){
+        Assertions.assertEquals(this.card.getValue(), testCardNumber);
     }
 }
--- src/test/java/cards/testPlayer.java
+++ src/test/java/cards/testPlayer.java
@@ -1,5 +1,20 @@
 package cards;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class testPlayer {
+
+    Player player;
+    int playerNumber;
+    Card[] cards;
+
+    @BeforeEach
+    @DisplayName("Generating Player")
+    void generatePlayer(){
+        
+    }
     
 }

cea28dd79914651419356418562f84cb9e887754
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2024-11-10 16:59:11 +0000

    CardGame Setup

--- src/main/java/cards/CardGame.java
+++ src/main/java/cards/CardGame.java
@@ -1,8 +1,13 @@
 package cards;
 
+import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class CardGame {
+public class CardGame extends Pack{
+    private static final ArrayList<Player> players = new ArrayList<>();
+    private static Queue<Card> PackofCards;
+
     public static void main(String[] args) {
         try{
             Scanner commandReader = new Scanner(System.in);
@@ -11,11 +16,37 @@ public class CardGame {
             System.out.println("Please enter the directory of your pack file: ");
             String packFile = commandReader.next();
             System.out.println(("You are playing with "+ numPlayers+ " players and the pack file"+ packFile));
+            PackofCards = Pack.readPack(packFile, numPlayers);
+            setupGame(numPlayers);
+            playGame();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
     }
-    public static void playGame(){
-        
+
+
+
+    public static void setupGame(int numPlayers){
+        CardDeck[] decks = new CardDeck[numPlayers];
+        for (int i = 0; i < numPlayers; i++){
+            decks[i] = new CardDeck(i);
+            players.set(i, new Player(i));
+        }
+        for(int i=0; i<4; i++){
+            for(int j=0; j<numPlayers; j++) {
+                players.get(j).addCard(i, PackofCards.remove());
+
+            };
+        while(!PackofCards.isEmpty()){
+            for(int k=0; k<numPlayers; k++) {
+                decks[k].addCard(PackofCards.remove());
+            }
+        }
+
+    }
+
+}
+    private static void playGame() {
     }
 }
---
+++ src/main/java/cards/Pack.java
@@ -0,0 +1,28 @@
+package cards;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Stream;
+
+abstract class Pack {
+    private static final Queue<Card> pack = new LinkedList<>();
+
+    public static Queue<Card> readPack(String packLocation, int NumPlayers){
+        FileEditor fileEditor = new FileEditor();
+        String[] packContentsStr = fileEditor.readFile(packLocation);
+        int[] packContents = Stream.of(packContentsStr).mapToInt(Integer::parseInt).toArray();
+        for (int i : packContents) {
+            pack.add(new Card(i));
+        }
+        if (validatePack(NumPlayers, pack)){
+            return pack;
+        } else {
+            throw new RuntimeException("Pack is not valid");
+        }}
+
+
+    public static Boolean validatePack(int NumPlayers, Queue<Card> packToValidate){
+        return pack.size() == 8*NumPlayers;
+
+    }
+}

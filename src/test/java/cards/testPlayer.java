4a849f4259ea1436cab73d2b5c169f962b275060
CliffHanger201 <sandy.hay@ntlworld.com>
2024-11-28 19:47:49 +0000

    test player commit

--- src/test/java/cards/testPlayer.java
+++ src/test/java/cards/testPlayer.java
@@ -1,9 +1,9 @@
 package cards;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 
 public class testPlayer {
 
@@ -17,13 +17,39 @@ public class testPlayer {
     @DisplayName("Generating Player")
     void generatePlayer(){
         this.playerNumber = (int) (Math.round(Math.random() * 100));
-        this.testLeftDeck = 
+        this.testLeftDeck = new CardDeck(1);
+        this.testRightDeck = new CardDeck(2);
     }
 
     @Test
     @DisplayName("Player Number ")
     void testPlayerNumber() {
-        
+        int testPlayerNumber = (int) (Math.round(Math.random() * 100));
+        this.player = new Player(testPlayerNumber, this.testLeftDeck, this.testRightDeck);
+        Assertions.assertEquals(player.getPlayerNumber(), testPlayerNumber);
     }
-    
+
+    @Test
+    @DisplayName("Adding a card")
+    void testPlayerCard() {
+        Card testCard = new Card((int) Math.round(Math.random()*100));
+        this.player.addCard(this.playerNumber, testCard);
+        Assertions.assertEquals(this.player.getCard(player.getAllCards().length - 1), testCard);
+    }
+
+    @Test
+    @DisplayName("Retrieve all player cards")
+    void multiplePlayerCards() {
+        this.player = new Player(this.playerNumber, this.testLeftDeck, this.testRightDeck);
+        Card[] playerDeck = new Card[4];
+        for (int i = 0; i < 5; i++) {
+            Card testCard = new Card((int) Math.round(Math.random()*100));
+            this.player.addCard(i, testCard);
+        }
+        Assertions.assertArrayEquals(this.player.getAllCards(), playerDeck);
+    }
+
+    @Test
+    @DisplayName()
+    void
 }

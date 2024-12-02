c165ad8e666af1f40627610cfbf1396e090ed5b6
CliffHanger201 <sandy.hay@ntlworld.com>
2024-12-02 13:47:59 +0000

    random commit v5

--- src/test/java/cards/testCardGame.java
+++ src/test/java/cards/testCardGame.java
@@ -1,9 +1,19 @@
 package cards;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class testCardGame {
-    
+
+    @Test
+    @DisplayName("Setting up the Game")
+    void testSetupGame() {
+
+    }
+
+    @Test
+    @DisplayName("Test playing the Game")
+    void testPlayGame() {
+
+    }
 }
